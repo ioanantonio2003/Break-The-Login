b6a1dc6dffeb14750d6012c4fe6e678e2d95f57f
antonio <ioan-antonio-332@deskly-vm.myguest.virtualbox.org>
2026-04-17 15:01:06 +0300

    AM adaugat partea de resetare a parolei

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template,make_response, redirect, url_for
 import sqlite3
+import time
 
 app = Flask(__name__)
+
+tokens = {}
 
 # ne conectam la baza de date
 def connect_to_database():
@@ -72,6 +75,55 @@ def dashboard():
         return redirect(url_for('login'))
         
     return render_template('dashboard.html')
+
+
+@app.route('/forgot', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        conn = connect_to_database()
+
+        #verificam daca exista emailul in baza de date
+        user = conn.execute('SELECT * FROM users WHERE email = ?', (email,)).fetchone()
+        conn.close()
+
+        if user:
+            # token predicitibil -> timpul curent 
+            token = str(int(time.time()))
+            tokens[token] = email #adaugam token pt email respectiv care nu expira niciodata si e reutilizabil
+            
+            link = f"http://127.0.0.1:5000/reset/{token}"
+            return f"<h3>Token trimis!</h3><p>Intra pe link-ul: <a href='{link}'>{link}</a></p>"
+        else:
+            return "<h3>Eroare: EMailul nu exista</h3><a href='/forgot'>Inapoi</a>"
+
+    #pt get        
+    return render_template('forgot.html')
+
+@app.route('/reset/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    # veririfcam daca token ul exista
+    if token not in tokens:
+        return "<h3>Eroare: Token gresit!</h3>"
+        
+    email = tokens[token]
+
+    if request.method == 'POST':
+        new_password = request.form['new_password']
+        
+        conn = connect_to_database()
+
+        #actuliazam parola
+        conn.execute('UPDATE users SET password_hash = ? WHERE email = ?', (new_password, email))
+        conn.commit()
+        conn.close()
+        
+        #dupa ce token-ul a fost utilizat nu il stergem in dictionar
+        return "<h3>Parola resetata</h3><a href='/login'>Login</a>"
+        
+    return render_template('reset.html', token=token)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
